38af05264e109aba32510e7753b8dab828e69b9d
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-05-19 20:49:06 +0530

    Project setup

---
+++ database/connect-db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDatabase = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      autoIndex: true,
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import dotenv from "dotenv";
+import { connectDatabase } from "./database/connect-db.js";
+
+// import { mergedResolvers } from "./resolvers/index.js";
+// import { mergedTypeDefs } from "./typeDefs/index.js";
+// import { connectDatabase } from "./database/connect-db.js";
+// import { context } from "./utils/context.js";
+
+dotenv.config();
+
+connectDatabase();
+
+const typeDefs = `
+type Query{
+  sayHi:String!
+}
+
+
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => {
+      `Hello WorldS`;
+    },
+  },
+};
+
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    introspection: true, // Enable introspection
+    // context,
+  });
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(morgan("dev"));
+
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
+}
+
+startServer();
